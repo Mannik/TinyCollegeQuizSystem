4e828dad4170894c626b3fcf953014838a42c728
Mannik <zbowen@null.net>
2021-01-28 22:09:56 -0600

    Created ClearForm method to clean up some code and quickly clear without reusing code.
    Added the ability to add a question to the quiz. Rough draft. Does not assign Key
    
    Signed-off-by: Zachary Bowen

--- TinyCollegeQuizSystem/NewQuizForm.cs
+++ TinyCollegeQuizSystem/NewQuizForm.cs
@@ -15,7 +15,8 @@ namespace TinyCollegeQuizSystem
     {
        
         SaveFileDialog sfd = new SaveFileDialog();
-        
+        string fileName = "placeholder.txt";
+
         public NewQuizForm()
         {
             InitializeComponent();
@@ -23,21 +24,50 @@ namespace TinyCollegeQuizSystem
 
         private void clearButton_Click(object sender, EventArgs e)
         {
-            questionTextBox.Clear();
-            option1TextBox.Clear();
-            option1RadioButton.Checked = false;
-            option2TextBox.Clear();
-            option2RadioButton.Checked = false;
-            option3TextBox.Clear();
-            option3RadioButton.Checked = false;
-            option4TextBox.Clear();
-            option4RadioButton.Checked = false;
-            questionTextBox.Focus();
+            ClearScreen();
         }
 
         private void addButton_Click(object sender, EventArgs e)
         {
-            
+            //use streamwrite to write to quiz file, create if does not exist.
+            using (StreamWriter sw = File.AppendText(fileName))
+            {
+                //check if entry boxes are blank, save if not
+                if (!((string.IsNullOrWhiteSpace(questionTextBox.Text)) || (string.IsNullOrWhiteSpace(option1TextBox.Text))
+                    || (string.IsNullOrWhiteSpace(option2TextBox.Text)) || (string.IsNullOrWhiteSpace(option3TextBox.Text)) || (string.IsNullOrWhiteSpace(option4TextBox.Text))))
+                {
+
+                    //check if a key is selected.
+                    if (!option1RadioButton.Checked && !option2RadioButton.Checked && !option3RadioButton.Checked && !option4RadioButton.Checked)
+                    {
+
+                        MessageBox.Show("You must select a Key");
+
+
+                    }
+                    else
+                    {
+
+                        //Save entries to file
+                        sw.WriteLine(questionTextBox.Text);
+                        sw.WriteLine(option1TextBox.Text);
+                        sw.WriteLine(option2TextBox.Text);
+                        sw.WriteLine(option3TextBox.Text);
+                        sw.WriteLine(option4TextBox.Text);
+                        //clear screen
+                        ClearScreen();
+                    }
+
+
+                }
+
+                //if 1 or all are blank, inform user that all fields must be entered.
+                else
+                {
+                    MessageBox.Show("All fields must be entered.");
+                }
+
+            }
         }
 
         private void NewQuizForm_Load(object sender, EventArgs e)
@@ -52,9 +82,22 @@ namespace TinyCollegeQuizSystem
             {
                 Stream fileStream = sfd.OpenFile();
                 fileStream.Close();
-
+                fileName = sfd.FileName;
             }
            
+        }
+        private void ClearScreen()
+        {
+            questionTextBox.Clear();
+            option1TextBox.Clear();
+            option1RadioButton.Checked = false;
+            option2TextBox.Clear();
+            option2RadioButton.Checked = false;
+            option3TextBox.Clear();
+            option3RadioButton.Checked = false;
+            option4TextBox.Clear();
+            option4RadioButton.Checked = false;
+            questionTextBox.Focus();
         }
     }
 }
